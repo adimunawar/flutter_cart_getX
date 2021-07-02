b5511d645a2e4fbe4a24f0652ae7f931732a9a7c
Adi Munawar <adimunawar77@gmail.com>
2021-07-02 09:19:33 +0700

    hapus

--- lib/views/shoping_page.dart
+++ lib/views/shoping_page.dart
@@ -105,68 +105,7 @@ class ShoppingPage extends StatelessWidget {
                           ))
                       .toList(),
                 );
-              })
-                  // Expanded(
-                  //   child: GetX<ShoppingController>(
-                  //     builder: (controller) {
-                  //       return ListView.builder(
-                  //         itemCount: controller.products.length,
-                  //         itemBuilder: (context, index) {
-                  //           return Card(
-                  //             margin: const EdgeInsets.all(12),
-                  //             child: Padding(
-                  //               padding: const EdgeInsets.all(16.0),
-                  //               child: Column(
-                  //                 crossAxisAlignment: CrossAxisAlignment.end,
-                  //                 children: [
-                  //                   Row(
-                  //                     mainAxisAlignment:
-                  //                         MainAxisAlignment.spaceBetween,
-                  //                     children: [
-                  //                       Column(
-                  //                         crossAxisAlignment:
-                  //                             CrossAxisAlignment.start,
-                  //                         children: [
-                  //                           Text(
-                  //                             '${controller.products[index].productName}',
-                  //                             style: TextStyle(fontSize: 24),
-                  //                           ),
-                  //                           Text(
-                  //                               '${controller.products[index].productDescription}'),
-                  //                         ],
-                  //                       ),
-                  //                       Text('\$${controller.products[index].price}',
-                  //                           style: TextStyle(fontSize: 24)),
-                  //                     ],
-                  //                   ),
-                  //                   ElevatedButton(
-                  //                     onPressed: () {
-                  //                       cartController
-                  //                           .addToCart(controller.products[index]);
-                  //                     },
-                  //                     child: Text('Add to Cart'),
-                  //                   ),
-                  //                   Obx(() => IconButton(
-                  //                         icon: controller
-                  //                                 .products[index].isFavorite.value
-                  //                             ? Icon(Icons.check_box_rounded)
-                  //                             : Icon(Icons
-                  //                                 .check_box_outline_blank_outlined),
-                  //                         onPressed: () {
-                  //                           controller.products[index].isFavorite
-                  //                               .toggle();
-                  //                         },
-                  //                       )),
-                  //                 ],
-                  //               ),
-                  //             ),
-                  //           );
-                  //         },
-                  //       );
-                  //     },
-                  //   ),
-                  // ),
-                  )
+              }))
             ],
           ),
         ),
